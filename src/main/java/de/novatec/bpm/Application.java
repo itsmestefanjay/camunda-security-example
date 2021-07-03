2a7b21619c9c7dc9aad4473d498fac0ef00cb2f5
Stefan Schultz <28778796+itsmestefanjay@users.noreply.github.com>
2021-07-03 19:41:18 +0200

    update app setup, remove complicated camunda configuration

--- src/main/java/de/novatec/bpm/Application.java
+++ src/main/java/de/novatec/bpm/Application.java
@@ -1,13 +1,12 @@
 package de.novatec.bpm;
 
 import de.novatec.bpm.config.CamundaConfig;
-import de.novatec.bpm.config.DataSourceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({CamundaConfig.class, DataSourceConfig.class})
+@Import({CamundaConfig.class})
 public class Application {
 
   public static void main(String[] args) {
--- src/main/java/de/novatec/bpm/config/CamundaConfig.java
+++ src/main/java/de/novatec/bpm/config/CamundaConfig.java
@@ -1,53 +1,27 @@
 package de.novatec.bpm.config;
 
-import java.io.IOException;
-import java.util.Collections;
-import javax.sql.DataSource;
-import org.camunda.bpm.engine.impl.jobexecutor.DefaultJobExecutor;
-import org.camunda.bpm.engine.impl.persistence.StrongUuidGenerator;
-import org.camunda.bpm.engine.spring.ProcessEngineFactoryBean;
-import org.camunda.bpm.engine.spring.SpringProcessEngineConfiguration;
-import org.camunda.spin.plugin.impl.SpinProcessEnginePlugin;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
+import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
+import org.camunda.bpm.spring.boot.starter.configuration.impl.AbstractCamundaConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.ResourcePatternResolver;
-import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
-public class CamundaConfig {
+public class CamundaConfig extends AbstractCamundaConfiguration {
 
-  @Autowired
-  ResourcePatternResolver resolver;
+  private final Logger logger = LoggerFactory.getLogger(CamundaConfig.class);
 
-  @Bean
-  SpringProcessEngineConfiguration springProcessEngineConfiguration(ApplicationContext applicationContext, DataSource camundaBpmDataSource, PlatformTransactionManager transactionManager) throws IOException {
-    SpringProcessEngineConfiguration config = new SpringProcessEngineConfiguration();
-    config.setDeserializationTypeValidationEnabled(true);
-    config.setDeserializationAllowedClasses("de.novatec.bpm.model.Car,de.novatec.bpm.dao.House");
-    config.setDeserializationTypeValidator(new DeserializeWhitelist());
-    config.setEnableXxeProcessing(false);
-    config.setTransactionManager(transactionManager);
-    config.setDataSource(camundaBpmDataSource);
-    config.setApplicationContext(applicationContext);
-    config.setDatabaseSchemaUpdate("true");
-    config.setProcessEnginePlugins(Collections.singletonList(new SpinProcessEnginePlugin()));
-    config.setDefaultSerializationFormat("application/json");
-    config.setJobExecutor(new DefaultJobExecutor());
-    config.setJobExecutorActivate(true);
-    config.setIdGenerator(new StrongUuidGenerator());
-    Resource[] resources = resolver.getResources("classpath:bpmn/process.bpmn");
-    config.setDeploymentResources(resources);
-    return config;
-  }
-
-  @Bean
-  ProcessEngineFactoryBean processEngineFactoryBean(SpringProcessEngineConfiguration springProcessEngineConfiguration) {
-    ProcessEngineFactoryBean processEngineFactoryBean = new ProcessEngineFactoryBean();
-    processEngineFactoryBean.setProcessEngineConfiguration(springProcessEngineConfiguration);
-    return processEngineFactoryBean;
+  @Override
+  public void preInit(ProcessEngineConfigurationImpl configuration) {
+    // disable telemetry
+    configuration.setInitializeTelemetry(false);
+    configuration.setTelemetryReporterActivate(false);
+    // enable deserialization type validation
+    configuration.setDeserializationTypeValidationEnabled(true);
+    configuration.setDeserializationAllowedClasses("de.novatec.bpm.model.Car,de.novatec.bpm.dao.House");
+    configuration.setDeserializationTypeValidator(new DeserializeWhitelist());
+    configuration.setEnableXxeProcessing(false);
+    logger.info("deserialization allowed for classes {}", configuration.getDeserializationAllowedClasses());
   }
 
 }
